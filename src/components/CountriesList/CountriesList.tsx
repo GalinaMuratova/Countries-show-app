216378aeb30bff4ff045b7cf670a4992707e6428
GalinaMuratova <ufkz200147@gmail.com>
2023-06-21 00:31:42 +0600

    Удалила лишние файлы. Подключила bootstrap и axios. Разделила приложение на два компонента.

---
+++ src/components/CountriesList/CountriesList.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CountriesList = () => {
+    return (
+        <div>
+            List
+        </div>
+    );
+};
+
+export default CountriesList;
---
+++ src/components/CountryInformation/CountryInformation.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CountryInformation = () => {
+    return (
+        <div>
+            Information
+        </div>
+    );
+};
+
+export default CountryInformation;
